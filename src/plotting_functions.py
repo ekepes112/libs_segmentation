ad3a9d474d892290bcda32580255065751056e19
ekepes112 <61498073+ekepes112@users.noreply.github.com>
2023-04-13 18:26:40 +0200

    functionality(EXP_fullSpectralClustering.ipynb): added embedding coordinates to rgb maps

--- src/plotting_functions.py
+++ src/plotting_functions.py
@@ -6,48 +6,52 @@ import logging
 db_logger = logging.getLogger(name='debug')
 db_logger.setLevel(logging.DEBUG)
 
+
 def plot_embedding(
     embedded_data,
     explained_variances=None,
     colors=None,
-    marker_size:float=2.,
-    return_figure:bool=False
+    marker_size: float = 2.,
+    return_figure: bool = False
 ):
     PERCENTAGE = 100
     CM = 1/2.54
     SUBPLOT_SIDE_SIZE = 5 * CM
 
-    if explained_variances is not None: explained_variances *= PERCENTAGE
+    if explained_variances is not None:
+        explained_variances *= PERCENTAGE
 
     embedding_dimension = embedded_data.shape[1]
     plot_combination_count = embedding_dimension * (embedding_dimension-1) // 2
     db_logger.debug(f'embedding combinations: {plot_combination_count}')
-    
+
     if plot_combination_count <= 9:
-        plot_count = plot_combination_count 
-        col_count = np.min([3,plot_count])
-        row_count = np.min([3,plot_count // col_count])
+        plot_count = plot_combination_count
+        col_count = np.min([3, plot_count])
+        row_count = np.min([3, plot_count // col_count])
         embedding_indices_to_plot = [
-            x 
-            for x 
+            x
+            for x
             in np.array(np.meshgrid(
                 np.arange(embedding_dimension - 1),
                 np.arange(embedding_dimension - 1) + 1
-            ))\
-              .T\
-              .reshape(-1,2) 
+            ))
+            .T
+            .reshape(-1, 2)
             if len(np.unique(x)) > 1
         ]
     else:
         plot_count = embedding_dimension
-        col_count = np.min([3,int(np.sqrt(plot_count))])
-        row_count = np.min([3,int(np.sqrt(plot_count))])
-        embedding_indices_to_plot = np.arange(0,col_count*row_count*2).reshape(-1,2)
+        col_count = np.min([3, int(np.sqrt(plot_count))])
+        row_count = np.min([3, int(np.sqrt(plot_count))])
+        embedding_indices_to_plot = np.arange(
+            0, col_count*row_count*2).reshape(-1, 2)
 
     db_logger.debug(f'plot_count: {plot_count}')
     db_logger.debug(f'cols, rows: {col_count}, {row_count}')
 
-    if colors is None: colors = [1] * len(embedded_data)
+    if colors is None:
+        colors = [1] * len(embedded_data)
 
     fig, ax = plt.subplots(
         ncols=col_count,
@@ -57,8 +61,8 @@ def plot_embedding(
 
     if col_count == 1:
         ax.scatter(
-            x=embedded_data[:,0],
-            y=embedded_data[:,1],
+            x=embedded_data[:, 0],
+            y=embedded_data[:, 1],
             s=marker_size,
             alpha=.25,
             c=colors,
@@ -74,10 +78,10 @@ def plot_embedding(
         ax.set_yticks([])
 
     elif col_count >= 2 and row_count == 1:
-        for ndx,plot_ndx in enumerate(range(col_count)):
+        for ndx, plot_ndx in enumerate(range(col_count)):
             ax[plot_ndx].scatter(
-                x=embedded_data[:,embedding_indices_to_plot[ndx][0]],
-                y=embedded_data[:,embedding_indices_to_plot[ndx][1]],
+                x=embedded_data[:, embedding_indices_to_plot[ndx][0]],
+                y=embedded_data[:, embedding_indices_to_plot[ndx][1]],
                 s=marker_size,
                 alpha=.25,
                 c=colors,
@@ -85,28 +89,28 @@ def plot_embedding(
             )
 
             x_lab = 'PC{} ({:.2f} %)'.format(
-                embedding_indices_to_plot[ndx][0] +1,
+                embedding_indices_to_plot[ndx][0] + 1,
                 explained_variances[embedding_indices_to_plot[ndx][0]]
             ) if explained_variances is not None else 'latent dim. {}'.format(
-                embedding_indices_to_plot[ndx][0] +1
+                embedding_indices_to_plot[ndx][0] + 1
             )
             y_lab = 'PC{} ({:.2f} %)'.format(
-                embedding_indices_to_plot[ndx][1] +1,
+                embedding_indices_to_plot[ndx][1] + 1,
                 explained_variances[embedding_indices_to_plot[ndx][1]]
             ) if explained_variances is not None else 'latent dim. {}'.format(
-                embedding_indices_to_plot[ndx][1] +1
-            )            
+                embedding_indices_to_plot[ndx][1] + 1
+            )
 
             ax[plot_ndx].set_xlabel(x_lab)
             ax[plot_ndx].set_ylabel(y_lab)
             ax[plot_ndx].set_xticks([])
             ax[plot_ndx].set_yticks([])
-    
+
     elif row_count >= 2:
-        for ndx,plot_ndx in enumerate(range(col_count * row_count)):
-            ax[plot_ndx // col_count,plot_ndx % row_count].scatter(
-                x=embedded_data[:,embedding_indices_to_plot[ndx][0]],
-                y=embedded_data[:,embedding_indices_to_plot[ndx][1]],
+        for ndx, plot_ndx in enumerate(range(col_count * row_count)):
+            ax[plot_ndx // col_count, plot_ndx % row_count].scatter(
+                x=embedded_data[:, embedding_indices_to_plot[ndx][0]],
+                y=embedded_data[:, embedding_indices_to_plot[ndx][1]],
                 s=marker_size,
                 alpha=.25,
                 c=colors,
@@ -114,35 +118,38 @@ def plot_embedding(
             )
 
             x_lab = 'PC{} ({:.2f} %)'.format(
-                embedding_indices_to_plot[ndx][0] +1,
+                embedding_indices_to_plot[ndx][0] + 1,
                 explained_variances[embedding_indices_to_plot[ndx][0]]
             ) if explained_variances is not None else 'latent dim. {}'.format(
-                embedding_indices_to_plot[ndx][0] +1
+                embedding_indices_to_plot[ndx][0] + 1
             )
             y_lab = 'PC{} ({:.2f} %)'.format(
-                embedding_indices_to_plot[ndx][1] +1,
+                embedding_indices_to_plot[ndx][1] + 1,
                 explained_variances[embedding_indices_to_plot[ndx][1]]
             ) if explained_variances is not None else 'latent dim. {}'.format(
-                embedding_indices_to_plot[ndx][1] +1
+                embedding_indices_to_plot[ndx][1] + 1
             )
 
-            ax[plot_ndx // col_count,plot_ndx % row_count].set_xlabel(x_lab)
-            ax[plot_ndx // col_count,plot_ndx % row_count].set_ylabel(y_lab)
-            ax[plot_ndx // col_count,plot_ndx % row_count].set_xticks([])
-            ax[plot_ndx // col_count,plot_ndx % row_count].set_yticks([])
+            ax[plot_ndx // col_count, plot_ndx % row_count].set_xlabel(x_lab)
+            ax[plot_ndx // col_count, plot_ndx % row_count].set_ylabel(y_lab)
+            ax[plot_ndx // col_count, plot_ndx % row_count].set_xticks([])
+            ax[plot_ndx // col_count, plot_ndx % row_count].set_yticks([])
 
     fig.tight_layout()
     fig.show()
 
-    if return_figure: return(fig)
-    else: return(None)
+    if return_figure:
+        return (fig)
+    else:
+        return (None)
+
 
 def plot_single_variable_map(
     plot_values,
     file_id
 ):
 
-    fig,ax = plt.subplots(1,1,figsize=(3,3))
+    fig, ax = plt.subplots(1, 1, figsize=(3, 3))
     ax.imshow(plot_values)
 
     ax.set_xticks([])
@@ -153,6 +160,6 @@ def plot_single_variable_map(
     fig.show()
 
     fig.savefig(
-        f'./temp/{file_id}{segment}.png',
+        f'./temp/{file_id}.png',
         transparent=True
     )
