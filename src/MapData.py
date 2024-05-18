edbf4424f5def330bd2f3f438905a408b57cefe1
ekepes112 <kepes.erik@gmail.com>
2024-05-18 22:19:14 +0200

    fix: indexing of denoised spectra

--- src/MapData.py
+++ src/MapData.py
@@ -559,7 +559,7 @@ class MapData:
         wavelet: pywt.Wavelet = pywt.Wavelet('rbio6.8'),
     ):
         denoised_arr = np.zeros_like(self.spectra)
-        for i in range(np.multiply.reduce(self.spectra.shape)):
+        for i in range(np.multiply.reduce(self.spectra.shape) -1):
             denoised_arr[i,:] = self._denoise_spectrum(
                 self.spectra[i,:],
                 level=level,
